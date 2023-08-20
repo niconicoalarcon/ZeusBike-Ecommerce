7d9e498ea427de69ebd84702324071c03205e7cd
Nicolas Alarcón <86640322+niconicoalarcon@users.noreply.github.com>
2023-08-19 21:09:58 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+//librerias 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'  
+
+//archivos propios
+import './css/style.css'
+import './css/styleNav.css'
+import './css/styleContacto.css'
